5fa744b4b2b9c2e9f9400b12127282e6c5589018
NevenPolsak <nevenpolsak1@gmail.com>
2022-04-10 13:54:33 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function hideBox(){
+    let x = document.getElementById("qoute")
+    if(x.style.display==="none"){
+        x.style.display="block"
+    }else{
+        x.style.display="none"
+    }
+}
